4eaa0eebdea53f7d1acfdeca5422e6ec61f7295b
rasbin <rasbin.thapa@learnsic.com>
2025-01-24 19:16:05 +0545

    refactoring

--- d1.go
+++ d1.go
@@ -1,44 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"sort"
-	"strconv"
-	"strings"
 )
 
 func main() {
-
-	file, err := os.Open("./d1_input_data.txt")
-	defer file.Close()
-
-	if err != nil {
-		fmt.Println("Error opening file: ", err)
-		return
-	}
-
 	var arr1, arr2 []int
 
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		parts := strings.Fields(line)
-
-		if len(parts) >= 2 {
-			num1, _ := strconv.Atoi(parts[0])
-			num2, _ := strconv.Atoi(parts[1])
-
-			arr1 = append(arr1, num1)
-			arr2 = append(arr2, num2)
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file: ", err)
-	}
+	file_data_to_arrays(&arr1, &arr2)
 
 	sort.Ints(arr1)
 	sort.Ints(arr2)
---
+++ read_input.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func file_data_to_arrays(arr1 *[]int, arr2 *[]int) {
+
+	file, err := os.Open("./d1_input_data.txt")
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+
+		if len(parts) >= 2 {
+			num1, _ := strconv.Atoi(parts[0])
+			num2, _ := strconv.Atoi(parts[1])
+
+			*arr1 = append(*arr1, num1)
+			*arr2 = append(*arr2, num2)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file: ", err)
+	}
+
+}
