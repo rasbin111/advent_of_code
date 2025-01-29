f6d6a54436f970013df3a62011e9411dd834e1f3
rasbin <rasbin.thapa@learnsic.com>
2025-01-29 18:56:03 +0545

    trying to solve d2

--- d2.go
+++ d2.go
@@ -8,54 +8,55 @@ import (
 	"strings"
 )
 
+func d2() {
+	file, err := os.Open("./d2_input_data.txt")
 
-func d2(){
-    file, err := os.Open("./d2_input_data.txt")
+	if err != nil {
+		fmt.Println("Error opening file", err)
+		return
+	}
+	scanner := bufio.NewScanner(file)
+	safeLineCount := 0
+	for scanner.Scan() {
+		line := scanner.Text() // get line
 
-    if (err != nil){
-        fmt.Println("Error opening file", err)
-        return 
-    }
-    scanner := bufio.NewScanner(file)
-    // safeLineCount := 0
-    for scanner.Scan(){
-        line := scanner.Text() // get line
-        // numbers := strings.Split(line, " ") // this also works
-        numbers := strings.Fields(line)
-        isIncreasing := true 
-        isDecreasing := true 
-        isSafe := true
-        max := strconv.Atoi(numbers[0])
-        min := strconv.Atoi(numbers[0])
-        prevValue := strconv.Atoi(numbers[0]) 
-        for _, value, err := strconv.Atoi(value) := range numbers{
-            if max <= value{
-                isIncreasing = false
-            } else {
-                max = value
-            }
-            if min >= value {
-                isDecreasing = false
-            } else {
-                min = value
-            }
-            if !isIncreasing && !isDecreasing{
-                isSafe = false
-                break
-            } else {
-                if value - prevValue > 3{
-                    isSafe = false
-                    break
-                } 
-           }
-            prevValue = value
-            safeLineCount += 1
-        }
+		numbers := strings.Fields(line) // numbers := strings.Split(line, " ") // this also works
+		isIncreasing := true
+		isDecreasing := true
+		isDecreasingOrIncreasing := true
+		isSafe := true
+		max, _ := strconv.Atoi(numbers[0])
+		min, _ := strconv.Atoi(numbers[0])
+		prevValue, _ := strconv.Atoi(numbers[0])
+		for _, value := range numbers {
+			num, _ := strconv.Atoi(value)
+			if max <= num {
+				isIncreasing = false
+			} else {
+				max = num
+			}
+			if min >= num {
+				isDecreasing = false
+			} else {
+				min = num
+			}
+			if !isIncreasing && !isDecreasing {
+				isDecreasingOrIncreasing = false
+				break
+			} else {
+				if num-prevValue > 3 {
+					isSafe = false
+					break
+				}
+			}
+			prevValue = num
+		}
 
-        fmt.Println("Safe Line Count", safeLineCount)
-    }
+		if isDecreasingOrIncreasing && isSafe {
+			safeLineCount += 1
+		}
 
-    
-    
-}   
+	}
+	fmt.Println("Safe Line Count", safeLineCount)
 
+}
