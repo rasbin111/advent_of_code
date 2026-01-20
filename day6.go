5205c3383d2f9d9040484efc04317c3943a3f1b4
rgt <rasbingodarthapa@gmail.com>
2026-01-20 17:32:53 +0545

    day6 part2 solved

--- day6.go
+++ day6.go
@@ -28,7 +28,7 @@ func readFile2SlNS(filepath string) [][]string {
 	return lines
 }
 
-func readFile2SlWithSpaceStr(filepath string, maxDigits int) [][]string {
+func readFile2SlWithSpaceStr(filepath string, maxSizes []int) [][]string {
 	file, err := os.Open(filepath)
 
 	if err != nil {
@@ -42,26 +42,42 @@ func readFile2SlWithSpaceStr(filepath string, maxDigits int) [][]string {
 	for scanner.Scan() {
 		line := scanner.Text()
 		row := []string{}
-		for i := 0; i <= len(line)-maxDigits; i = i + maxDigits + 1 {
-			num := line[i : i+maxDigits]
+		i := 0
+		for j := 0; j <= len(line)-maxSizes[len(maxSizes)-1]; j++ {
+			maxDigits := maxSizes[i]
+			num := line[j : j+maxDigits]
 			row = append(row, num)
+			j = j + maxDigits
+			i++
 		}
 		lines = append(lines, row)
 	}
 	return lines
 }
 
-func maxLengthOfNum(lines [][]string) int {
-	maxLen := len(lines[0][0])
-
-	for i := range lines {
-		for j := range lines[0] {
-			if maxLen < len(lines[i][j]) {
+func maxLengthOfNum(lines [][]string) []int {
+	maxSize := []int{}
+	for j := range lines[0] {
+		maxLen := 0
+		for i := range lines {
+			str := lines[i][j]
+			lenStr := len(str)
+			if maxLen < lenStr {
 				maxLen = len(lines[i][j])
 			}
 		}
+		maxSize = append(maxSize, maxLen)
 	}
-	return maxLen
+	return maxSize
+}
+
+func strToIntOrDefZero(s string) int {
+	i, err := strconv.Atoi(strings.TrimSpace(s))
+
+	if err != nil {
+		return 0
+	}
+	return i
 }
 
 func D6P1(grid [][]string) {
@@ -96,38 +112,65 @@ func D6P1(grid [][]string) {
 	fmt.Println("Total amount: ", totalCalc)
 }
 
-func D6P2(maxDigits int) {
+func D6P2Calculation(lines [][]string) {
+	totalCalc := 0
+	for _, value := range lines {
+		symbol := strings.TrimSpace(value[len(value)-1])
+		calc := 0
+		switch symbol {
+		case "*":
+			calc = 1
+			for _, fv := range value[:len(value)-1] {
+				num := strToIntOrDefZero(fv)
 
-	grid := readFile2SlWithSpaceStr("inputs/day6.txt", maxDigits)
-	fmt.Println(grid)
-	nums := make([]int, len(grid[0]))
-	_ = nums
+				calc *= num
+			}
+		case "+":
+			for _, fv := range value[:len(value)-1] {
+				num := strToIntOrDefZero(fv)
+
+				calc += num
+			}
+		}
+		totalCalc += calc
+	}
+	fmt.Println("Total amount: ", totalCalc)
+
+}
+
+func D6P2(maxSizes []int) {
+
+	grid := readFile2SlWithSpaceStr("inputs/day6.txt", maxSizes)
+	nums := [][]string{}
+
 	for j := len(grid[0]) - 1; j >= 0; j-- {
 		lastRowIdx := len(grid) - 1
 		symbol := grid[lastRowIdx][j]
-		// fmt.Println("Symbol: ", symbol)
-		// countTotalNums := len(grid[0][j])
-		tempNums := make([]int64, maxDigits)
-		// _ = tempNums
-		_ = symbol
-		num := 0
-		for i := 0; i < len(grid)-1; i++ {
-			num := grid[i][j]
-			for d := 0; d < maxDigits; d++ {
-				n, err := strconv.ParseInt(string(num[d]), 10, 64)
 
-				if err != nil {
-					log.Fatal(err)
-				}
-				num := n
+		maxDigits := maxSizes[j]
+		tempNums := make([]string, maxDigits+1)
+		tempNums[maxDigits] = symbol
+		_ = tempNums
+		_ = symbol
+
+		for i := 0; i < len(grid)-1; i++ {
+			selectedNum := grid[i][j]
+			// mf := 1
+			for d := 0; d < maxDigits; d++ {
+				num := tempNums[d]
+				digit := string(selectedNum[d])
+				num = num + digit
+				tempNums[d] = num
+				// num = n
 			}
 		}
+		nums = append(nums, tempNums)
 	}
-	// fmt.Println("Total amount: ", totalCalc)
+	D6P2Calculation(nums)
 }
 
 func Day6() {
 	grid := readFile2SlNS("inputs/day6.txt")
-	maxDigits := maxLengthOfNum(grid)
-	D6P2(maxDigits)
+	maxSizes := maxLengthOfNum(grid)
+	D6P2(maxSizes)
 }
